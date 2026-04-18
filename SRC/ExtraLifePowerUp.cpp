5e60056a3eec2ba9f726cf7f3c9f715e7edec811
Stelios Michaelides <stemichaelides@gmail.com>
2026-04-18 20:12:40 +0300

    Add ExtraLifePowerUp subcclass

---
+++ SRC/ExtraLifePowerUp.cpp
@@ -0,0 +1,8 @@
+#include "ExtraLifePowerUp.h"
+
+ExtraLifePowerUp::ExtraLifePowerUp()
+    : PowerUp("ExtraLifePowerUp", POWERUP_EXTRA_LIFE)
+{
+}
+
+ExtraLifePowerUp::~ExtraLifePowerUp() {}
---
+++ SRC/ExtraLifePowerUp.h
@@ -0,0 +1,13 @@
+#ifndef __EXTRALIFEPOWERUP_H__
+#define __EXTRALIFEPOWERUP_H__
+
+#include "PowerUp.h"
+
+class ExtraLifePowerUp : public PowerUp
+{
+public:
+    ExtraLifePowerUp();
+    virtual ~ExtraLifePowerUp();
+};
+
+#endif
